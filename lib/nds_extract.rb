e616f06f8d1f55d917926392c328d40685338a61
Fitzhugh <j.rfitzhugh@gmail.com>
2019-10-29 11:57:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -50,8 +50,8 @@ def total_gross(source)
   total_index = 0
   
   while total_index < list_of_directors(source).size do
-    
-    grand_total += directors_totals(list_of_directors(list))
+    directors = list_of_directors(source)
+    grand_total += directors_totals(director)
     
     total_index += 1
   end
